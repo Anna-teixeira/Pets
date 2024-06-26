7c6bf19e017878317ebfaa1f391f168155f88696
Anna-teixeira <anna.teixeira12@gmail.com>
2024-06-26 13:13:43 +0100

    Comecei de novo

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Adoção Pets</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/images/minilogo.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="index-page">
+
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center">
+     
+      <a href="index.html" class="logo d-flex align-items-center me-auto">
+        <img src="assets/img/images/logo.png" alt="">
+        <h1 class="sitename"></h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="index.html#hero" class="">Home</a></li>
+          <li><a href="index.html#about">Sobre</a></li>
+          <li><a href="index.html#features">Adote</a></li>
+          <li><a href="index.html#pricing">Contribua</a></li>
+          <li><a href="index.html#contact">Contatos</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+  </header>
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section">
+      <div class="hero-bg">
+        <img src="assets/img/images/fotoinicioadote.jpg" alt="">
+      </div>
+      <div class="container text-left">
+        <div class="d-flex flex-column justify-content-left align-items-left">
+          <h1 data-aos="fade-up">Adoção</h1>
+          <p data-aos="fade-up" data-aos-delay="100">O processo de adoção da ONG é dividido em três etapas: escolher o pet desejado, entrar em contato expressando interesse e confirmando o mascote escolhido, e por fim, uma entrevista de confirmação para que todos tenham total entendimento das responsabilidades via adotar aquele pet, assim como a testar a identidade da pessoa que se candidatou à posição de tutor. Infelizmente, apenas demonstrar interesse não garante a adoção, pois é necessário verificar que cada um preenche-se requisitos necessários para o acolhimento do animal selecionado.<br></p>
+          <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
+         
+      </div>
+
+    </section><!-- /Hero Section -->
+
+    <!-- Adoption Section -->
+    <section id="Adoption" class="Adoption section">
+
+      <div class="container">
+
+        <div class="row g-5">
+
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="Adoption-item position-relative">
+              <div>
+                <h3>Lina</h3>
+                <a href="Adoption-details.html" class="read-more stretched-link">Learn More <i class="bi bi-arrow-right"></i></a>
+              </div>
+            </div>
+          </div><!-- End Adoption Item -->
+
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="Adoption-item position-relative">
+              <div>
+                <h3>Nick</h3>
+                <a href="Adoption-details.html" class="read-more stretched-link">Learn More <i class="bi bi-arrow-right"></i></a>
+              </div>
+            </div>
+          </div><!-- End Adoption Item -->
+
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="300">
+            <div class="Adoption-item position-relative">
+              <div>
+                <h3>Toddy</h3>
+                <a href="Adoption-details.html" class="read-more stretched-link">Learn More <i class="bi bi-arrow-right"></i></a>
+              </div>
+            </div>
+          </div><!-- End Adoption Item -->
+
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="400">
+            <div class="Adoption-item position-relative">
+              <div>
+                <h3>Dino</h3>
+                <a href="Adoption-details.html" class="read-more stretched-link">Learn More <i class="bi bi-arrow-right"></i></a>
+              </div>
+            </div>
+          </div><!-- End Adoption Item -->
+
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="500">
+            <div class="Adoption-item position-relative">
+              <div>
+                <h3>Garfield</h3>
+                <a href="Adoption-details.html" class="read-more stretched-link">Learn More <i class="bi bi-arrow-right"></i></a>
+              </div>
+            </div>
+          </div><!-- End Adoption Item -->
+
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="600">
+            <div class="Adoption-item position-relative">
+              <div>
+                <h3>Jack</h3>
+                <a href="Adoption-details.html" class="read-more stretched-link">Learn More <i class="bi bi-arrow-right"></i></a>
+              </div>
+            </div>
+          </div><!-- End Adoption Item -->
+         
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="600">
+            <div class="Adoption-item position-relative">
+              <div>
+                <h3>Baunilha</h3>
+                <a href="Adoption-details.html" class="read-more stretched-link">Learn More <i class="bi bi-arrow-right"></i></a>
+              </div>
+            </div>
+          </div><!-- End Adoption Item -->
+
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="600">
+            <div class="Adoption-item position-relative">
+              <div>
+                <h3>Alo</h3>
+                <a href="Adoption-details.html" class="read-more stretched-link">Learn More <i class="bi bi-arrow-right"></i></a>
+              </div>
+            </div>
+          </div><!-- End Adoption Item -->
+
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="600">
+            <div class="Adoption-item position-relative">
+              <div>
+                <h3>Kenai</h3>
+                <a href="Adoption-details.html" class="read-more stretched-link">Learn More <i class="bi bi-arrow-right"></i></a>
+              </div>
+            </div>
+          </div><!-- End Adoption Item -->
+
+        </div>
+
+      </div>
+
+    </section><!-- /Adoption Section -->
+
+   
+  </main>
+
+  <footer id="footer" class="footer position-relative">
+
+    <div class="container footer-top">
+      <div class="row gy-4">
+        <div class="col-lg-4 col-md-6 footer-about">
+          <a href="index.html" class="logo d-flex align-items-center">
+            <span class="sitename">Adote um Vira Lata</span>
+          </a>
+          <div class="footer-contact pt-3">
+            <p>A108 Adam Street</p>
+            <p>Curitiba, PR</p>
+            <p class="mt-3"><strong>Phone:</strong> <span>+55 98172 6659</span></p>
+            <p><strong>Email:</strong> <span>info@example.com</span></p>
+          </div>
+          <div class="social-links d-flex mt-4">
+            <a href=""><i class="bi bi-twitter-x"></i></a>
+            <a href=""><i class="bi bi-facebook"></i></a>
+            <a href=""><i class="bi bi-instagram"></i></a>
+            <a href=""><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-md-3 footer-links">
+          <h4>Links úteis</h4>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Terms of service</a></li>
+            <li><a href="#">Privacy policy</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-3 footer-links">
+          <h4>Nos ajude</h4>
+          <ul>
+            <li><a href="#">Adote</a></li>
+            <li><a href="#">Contribua</a></li>
+            <li><a href="#">Voluntariado</a></li>
+            <li><a href="#">Contato</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-4 col-md-12 footer-newsletter">
+          <h4>Newsletter</h4>
+          <p>Subscreva para nos aujudar ao máximo!</p>
+          <form action="forms/newsletter.php" method="post" class="php-email-form">
+            <div class="newsletter-form"><input type="email" name="email"><input type="submit" value="Subscribe"></div>
+            <div class="loading">Loading</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Your subscription request has been sent. Thank you!</div>
+          </form>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container copyright text-center mt-4">
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">Anna Teixeira</strong><span>All Rights Reserved</span></p>
+      <div class="credits">
+      
+        Designed by <a href="https://anna-teixeira.github.io/">Anna Teixeira</a>
+      </div>
+    </div>
+
+  </footer>
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
